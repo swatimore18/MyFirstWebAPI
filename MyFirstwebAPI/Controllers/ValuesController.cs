c3c0624c827303e203ccd9ac470870122adc6608
Veer <Veer@example.com>
2025-08-19 21:26:38 -0500

    Add project files.

---
+++ MyFirstwebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstwebAPI.Controllers
+{
+   
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route ("api/[controller]")]
+        public IActionResult Get() 
+        {
+            return Ok(new { message = "Hello from Azure-ready API" });
+        }
+    }
+}
